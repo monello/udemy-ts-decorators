db5d1a18097255ebcee82f61c7756284ae52bb88
Morne Louw <louwmo01@stepstone.com>
2022-03-10 16:46:37 +0200

    Investigtor the order or execution when adding multple decorators to a class

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@
 // - This will be a class decorator so it only receives 1 argument
 // - The argument here is a pointer to the constructor function of the target-class (the class you are applying the decorator to)
 const Logger = (constructor: Function) => {
-    console.log('Logging...');
+    console.warn('Hello from Logger decorator');
     // Now that the argument is added to the function, we are now able to use this as a decorator
     console.log(constructor);
 }
@@ -11,10 +11,13 @@ const Logger = (constructor: Function) => {
 // A factory decorator return a function
 // This allows us to pass in our own variables to the main decorator functiomn
 const LoggerFactory = (logString: string) => {
+    console.warn('Hello from "factory" Factory decorator');
+
     // - Decorators get passed different arguments depending on where you use it
     // - This will be a class decorator so it only receives 1 argument
     // - The argument here is a pointer to the constructor function of the target-class (the class you are applying the decorator to)
     return (constructor: Function) => {
+        console.warn('Hello from "decorator" Factory decorator')
         console.log(logString);
         // Now that the argument is added to the function, we are now able to use this as a decorator
         console.log(constructor);
@@ -23,10 +26,14 @@ const LoggerFactory = (logString: string) => {
 
 // Another Factory Class Decorator, with a more useful example
 const withTemplate = (template: string, hookId: string) => {
+    console.warn('Hello from "factory" withTemplate decorator');
+
     // Here we had to change the argument type to any in order to use the "new" keyword below
     // TS does not know that the Function being passed is a valid constructor function, so we need to go very wide
     //  on our typing here and set it to any
     return (constructor: any) => {
+        console.warn('Hello from "decorator" withTemplate decorator');
+
         // Insert the template into a target element in the DOM
         const targetElem = document.getElementById(hookId);
         // Here we can instantiate (use the new-keyword) a person object from the constructor function
