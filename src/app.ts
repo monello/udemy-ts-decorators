fb94778c0ea192678831236dc54df580ad76702c
Morne Louw <louwmo01@stepstone.com>
2022-03-10 15:39:03 +0200

    Basic class decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// Logger Decorator
+// - Decorators get passed different arguments depending on where you use it
+// - This will be a class decorator so it only receives 1 argument
+// - The argument here is a pointer to the constructor function of the target-class (the class you are applying the decorator to)
+const Logger = (constructor: Function) => {
+    console.log('Logging...');
+    // Now that the argument is added to the function, we are now able to use this as a decorator
+    console.log(constructor);
+}
+
+// When you add the function in this way TS will know that you mean to use it as a decorator and apply the proper syntax checks
+@Logger
 class Person {
     name = "Morne";
 
